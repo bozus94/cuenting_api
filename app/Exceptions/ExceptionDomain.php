a19ecd4963090cd1251e813e10c656f1856480cd
Bryan Ortega <bj_ortega@hotmail.com>
2025-08-30 00:44:01 +0200

    create-exception-domain

--- app/Exceptions/ExceptionDomain.php
+++ app/Exceptions/ExceptionDomain.php
@@ -2,9 +2,24 @@
 
 namespace App\Exceptions;
 
+use App\Traits\CuentingResponse;
 use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Request;
 
-class ExceptionDomain extends Exception
+abstract class ExceptionDomain extends Exception
 {
-    //
+    use CuentingResponse;
+
+    public function __construct(public readonly string $codeDomain, public readonly int $httpStatus = 400, public readonly array $errors = [], string $message = '')
+    {
+        parent::__construct($message ?: $codeDomain, $httpStatus);
+    }
+
+    public function render(Request $request): JsonResponse
+    {
+        return $this->failure($this->codeDomain, $this->mapMessages(), $this->errors, $this->httpStatus);
+    }
+
+    abstract function mapMessages(): string;
 }
